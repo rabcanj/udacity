3284eb16f1a16b40decd0673143db2bbee12a659
Juraj Rabcan <rabcanj@gmail.com>
2019-09-06 01:53:34 +0200

    task 2

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Logs Analysis
 ### How to run me
 The script uses python 2.7 and postgre database. You can use VM machine configuration available here: [vagrant](https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip), where everything is installed.
-Next:
+
 - download the [database](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
 - unzip and import data into the postgre database
 ```sh
 $ psql -f newsdata.sql -d news
 ```
-Run the script with the following command:
+Execute script:
 ```sh
 $ python3 main.py
 ```
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     db = psycopg2.connect(dbname="news", user="", password="")
     cur = db.cursor()
-
+    #TASK 1
     cur.execute("""
         SELECT title, COUNT (title) FROM ARTICLES
         LEFT JOIN LOG ON LOG.PATH like '%'  || ARTICLES.SLUG
@@ -15,7 +15,20 @@ if __name__ == '__main__':
         LIMIT 3
     """)
     res = cur.fetchall()
-    print()
     print("1. What are the most popular three articles of all time?:")
     for i in res:
         print("{name} - {views} views".format(name = i[0], views = i[1]))
+    #TASK 2
+    cur.execute("""
+        SELECT  COUNT (title), author, authors.name FROM ARTICLES
+        LEFT JOIN LOG ON LOG.PATH like '%'  || ARTICLES.SLUG
+        LEFT JOIN authors ON authors.id = ARTICLES.author
+        GROUP BY  author,authors.name
+        ORDER BY COUNT (title) DESC;
+    """)
+    res = cur.fetchall()
+    print
+    print("2. Who are the most popular authors of all times?:")
+    for i in res:
+        print("{name} - {views} views".format(name = i[0], views = i[2]))
+    #TASK 3
