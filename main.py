cb787fc16a3e8f3fc8c4c599815217e03cf70de4
Juraj Rabcan <rabcanj@gmail.com>
2019-09-06 01:26:45 +0200

    first task done

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+
+import psycopg2
+import time
+
 if __name__ == '__main__':
-    print 'hello'
+
+    db = psycopg2.connect(dbname="news", user="", password="")
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT title, COUNT (title) FROM ARTICLES
+        LEFT JOIN LOG ON LOG.PATH like '%'  || ARTICLES.SLUG
+        GROUP BY title
+        ORDER BY COUNT (title) DESC
+        LIMIT 3
+    """)
+    res = cur.fetchall()
+    print()
+    print("1. What are the most popular three articles of all time?:")
+    for i in res:
+        print("{name} - {views} views".format(name = i[0], views = i[1]))
