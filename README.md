2f3dfea1137970763da76475e44b50bc9e465170
vagrant <vagrant@vagrant.vm>
2019-09-06 01:20:31 +0000

    3rd task done

--- README.md
+++ README.md
@@ -3,11 +3,29 @@
 The script uses python 2.7 and postgre database. You can use VM machine configuration available here: [vagrant](https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip), where everything is installed.
 
 - download the [database](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
-- unzip and import data into the postgre database
+- import data file (newsdata.sql) into the postgre database
 ```sh
 $ psql -f newsdata.sql -d news
 ```
 Execute script:
 ```sh
 $ python main.py
+```
+
+Example:
+```sh
+$ python main.py
+1. What are the most popular three articles of all time?:
+Candidate is jerk, alleges rival - 338647 views
+Bears love berries, alleges bear - 253801 views
+Bad things gone, say good people - 170098 views
+
+2. Who are the most popular authors of all times?:
+507594 - Ursula La Multa views
+423457 - Rudolf von Treppenwitz views
+170098 - Anonymous Contributor views
+84557 - Markoff Chaney views
+
+3.  On which days did more than 1% of requests lead to errors:
+Jul 17, 2016 - 2.3 views
 ```
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 import psycopg2
 import time
+
 
 if __name__ == '__main__':
 
@@ -31,4 +31,28 @@ if __name__ == '__main__':
     print("2. Who are the most popular authors of all times?:")
     for i in res:
         print("{name} - {views} views".format(name = i[0], views = i[2]))
-    #TASK
+    #TASK 3
+    cur.execute("""
+        SELECT TO_CHAR(date_trunc, 'Mon DD, YYYY'), round(errorpercentage,1)  from (
+            SELECT
+                date_trunc('day',time),
+                STATUS,
+                count(status),
+                LOG2.total,
+                (count(status)::numeric*100)/LOG2.total::numeric as errorpercentage
+            FROM LOG
+            LEFT JOIN (
+                    SELECT date_trunc('day',time) as day_, count(status) as total
+                    FROM LOG GROUP BY
+                    date_trunc('day',time)
+                ) as LOG2 on date_trunc('day',time) = LOG2.day_
+            GROUP BY date_trunc('day',time), STATUS, LOG2.TOTAL
+            HAVING (LOG2.TOTAL::numeric/100) < count(status)::numeric
+            AND status = '404 NOT FOUND'
+        ) as foo;
+    """)
+    print
+    print("3.  On which days did more than 1% of requests lead to errors:")
+    res = cur.fetchall()
+    for i in res:
+        print("{date} - {errorpercentage} views".format(date = i[0], errorpercentage = i[1]))
